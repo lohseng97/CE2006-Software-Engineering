9ba9b5e4cdfd1932d2dbbbd3e993a7f2894e5237
lohseng97 <chewlohseng@live.com>
2022-01-28 18:17:07 +0800

    Upload Finalized Report

--- medicine_reminders/android/app/src/main/kotlin/com/example/medicine_reminders/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.medicine_reminders
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
